93e665214f6cdc8445760b9f7bd885789603863f
Renato Ribeiro <renatoribroman@gmail.com>
2017-09-22 20:48:08 -0300

    prevent sort to mutate

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const keys = obj => Object.keys(obj)
 export const map = fn => array => array.map(fn)
 export const filter = fn => array => array.filter(fn)
-export const sort = fn => array => array.sort(fn)
+export const sort = fn => array => [...array].sort(fn)
 export const head = array => array[0]
 export const gte = num1 => num2 => num1 >= num2
 export const lte = num1 => num2 => num1 <= num2
